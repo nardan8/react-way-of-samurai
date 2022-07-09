8090b0d13deba59655b3480033b817fe60d21939
Nariman <daniyarov1988@gmail.com>
2022-07-09 22:53:56 +0600

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import mylogo from "../images/my-logo.png";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src={mylogo} alt="My logo" />s
+    </header>
+  );
+};
+
+export default Header;
